21cfea64846f0169fe23aa7ae9c68ebb197cf1b0
kasiaknapowska <k.knapowska@gmail.com>
2023-06-22 13:10:28 +0200

    jak-dojechac page - locator & address

--- src/pages/jak-dojechac.js
+++ src/pages/jak-dojechac.js
@@ -4,12 +4,20 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import GoogleMap from "../components/elements/googleMap/googleMap"
+import Locator from "../images/logo/Lokalizator.svg"
 
 const JakDojechac = () => (
   <Layout>
     <section className="topSection">
-    <div className="container">
+    <div className="container dflex50" style={{justifyContent: "space-between", alignItems: "center"}}>
       <h1>Jak do nas dojechać?</h1>
+      <div style={{display: "flex", gap: "1.5rem"}}>
+        <img src={Locator} alt="lokalizator" className="locator"/>
+        <div>
+        <p>Katowice, ul. Czajek 5</p>
+        <p>Współrzędne GPS (DD): 50.235478 / 19.007185</p>
+        </div>
+      </div>
     </div>
     </section>
     <GoogleMap />
